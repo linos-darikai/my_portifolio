9eea19094d136db216d3d8e20568b61c9065da4e
Avalance546 <138253858+Avalance546@users.noreply.github.com>
2025-03-22 22:36:56 +0000

    Fixed the navigation bar on scrolling

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Mad thing");
+
+let prevScroll = window.scrollY;
+
+
+function scrollReg(){
+    let currScroll = window.scrollY;
+    let navBar = document.getElementById("navbar");
+    if(currScroll - prevScroll < 0 ){
+        navBar.style.top = 0;
+        navBar.style.backgroundColor = "rgb(10, 25, 47, 0.998)";
+
+    }
+    else{
+        navBar.style.top = "-60px";
+    }
+    prevScroll = currScroll;
+}
+
+window.onscroll = scrollReg;
